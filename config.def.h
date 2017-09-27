b3e3298926a28d577f241a94d58b35960f4ee58f
joshua stein <jcs@jcs.org>
2017-09-27 14:48:51 -0500

    config.def.h: use dark gray for INIT color, unset failonclear

--- config.def.h
+++ config.def.h
@@ -3,13 +3,13 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
+	[INIT] =   "#444444",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
 
 /* how many seconds to wait before turning screen off again */
 static const int dpmstimeout = 5;
